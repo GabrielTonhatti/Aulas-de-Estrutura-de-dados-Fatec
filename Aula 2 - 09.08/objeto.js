f97b64ca7d63ec7be86f515eb9610a3b20d911e9
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2021-08-10 15:22:06 -0300

    First Commit

---
+++ Aula 2 - 09.08/objeto.js
@@ -0,0 +1,88 @@
+/* 
+    Objetos são estruturas da linguagem JS que permitem armazenar
+    múltiplos valores em uma variável, tal como os vetores. No
+    entanto, ao contrário destes, as posições dos valores são
+    NOMEADAS, não NUMERADAS.
+*/
+
+// Criação de um objeto vazio
+let objeto1 = new Object()  // Forma tradicional
+let objeto2 = {}            // Forma moderna
+
+// Criação objetos já com propriedades
+// Uma propriedade é um par formado pelo nome da propriedade
+// e o valor dela, separados por dois pontos
+// Propriedades são também chamadas de atributos
+let pessoa = {
+    nome: 'Juracir Junqueira',
+    sexo: 'M',
+    dataNasc: '2000-09-18',
+    ocupacao: 'estudante',
+    // Quando nomes de propriedades contêm espaços ou caracteres acentuados,
+    // eles devem vir ENTRE ASPAS
+    'cidade de origem': 'Franca/SP',
+    'série': '3º C',
+    gostos: ['jogar', 'comer doces', 'dormir']
+}
+
+// Acessando as propriedades de um objeto
+console.log(pessoa.nome)
+console.log(pessoa.gostos)
+
+// Quando o nome da propriedade contém espaços ou acentos,
+// devemos acessá-la usando a SINTAXE DOS COLCHETES`1
+console.log(pessoa['cidade de origem'])
+console.log(pessoa['série'])
+
+// A SINTAXE DOS COLCHETES sempre funciona!
+console.log(pessoa['dataNasc'])
+console.log(pessoa['ocupacao'])
+
+// Uma consequência da sintaxe dos colchetes é a possibilidade de
+// ter nomes de propriedades de dentro de variáveis
+
+console.log('------------------------------------------------')
+
+// Vetor com o nome das propriedades
+let props = ['nome', 'sexo', 'dataNasc', 'ocupacao']
+
+for (let p of props) {
+    console.log(pessoa[p])
+}
+// Acrescentando novas propriedades a um objeto existente
+pessoa.email = 'juracirj@gmail.com'
+pessoa['telefone contato'] = '(16) 99999-8888'
+console.log(pessoa) // Exibindo o objeto inteiro
+
+console.log('------------------------------------------------')
+
+// Iniciando um objeto vazio e criando as propriedades depois
+let carro = {}
+carro.modelo = 'Volkswagem';
+carro.marca = 'Fusca';
+carro.cor = 'Preto';
+carro.ano = 1969;
+carro['nome proprietário'] = 'Juscelina Jordão';
+
+console.log(carro);
+
+console.log('------------------------------------------------')
+console.log('Exibindo os nomes das propriedades do objeto "pessoa":');
+
+// for...in: retorna os nomes das propriedades de um objeto
+for(let prop in pessoa) {
+    console.log(prop)
+}
+
+console.log('------------------------------------------------')
+console.log('Exibindo os nomes das propriedades do objeto "carro":');
+
+for(let atrib in carro) {
+    console.log(atrib)
+}
+
+// for...in pode ser utilizado juntamente com a sintaxe de colchetes
+// para recuperar o valor de todas as propriedades de um objeto
+for(let p in pessoa) {
+    console.log(`${p} -> ${pessoa[p]}`)
+}
